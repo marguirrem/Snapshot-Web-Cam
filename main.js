837cfe4ecbe47f41dd77cc9a771d99526f5dc291
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2017-01-04 00:35:19 -0600

    show the video on canvas

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 ;(function(){
 	if(!!(navigator.getUserMedia||navigator.webkitGetUserMedia)){
 		navigator.webkitGetUserMedia({video:true},function(localMediaStream){
-			document.getElementById('video').src = window.URL.createObjectURL(localMediaStream)
+			var video = document.getElementById('video');
+			video.src = window.URL.createObjectURL(localMediaStream);
+			
+			var canvas = document.getElementById('canvas');
+			var context = canvas.getContext('2d');
+			setInterval(function(){
+				context.drawImage(this.video ,0, 0);
+			},1000/30)
 		},function (error) {
 			console.log(error);
 		})
